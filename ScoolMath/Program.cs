113e50222e1c5a7c75d7a8499e87f7e8bda4fd4e
darkwin <darkwin.pro@gmail.com>
2023-07-01 20:21:41 +0300

    Initial commit

---
+++ ScoolMath/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Answers
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Считываем числитель дроби.
+            var numerator = Convert.ToInt32(Console.ReadLine());
+            // Считываем знаменатель дроби.
+            var denominator = Convert.ToInt32(Console.ReadLine());
+
+            // Копируем значения числителя и знаменателя во временные переменные.
+            var tempNumerator = numerator;
+            var tempDenominator = denominator;
+
+            // Используем алгоритм Евклида для нахождения НОД:
+            // Пока числитель и знаменатель не равны нулю.
+            while (tempNumerator != 0 && tempDenominator != 0)
+            {
+                // Если числитель больше знаменателя.
+                if (tempNumerator > tempDenominator)
+                {
+                    // Числителю присваиваем значение, равное остатку от деления числителя на знаменатель.
+                    tempNumerator %= tempDenominator;
+                }
+                else
+                {
+                    // Иначе, знаменателю присваиваем значение, равное остатку от деления знаменателя на числитель.
+                    tempDenominator %= tempNumerator;
+                }
+            }
+
+            // Вычисляем НОД.
+            var greatestCommonDivisor = tempNumerator + tempDenominator;
+            
+            // Вычисляем новый числитель (после сокращения дроби).
+            var newNumerator = numerator / greatestCommonDivisor;
+            // Вычисляем новый знаменатель (после сокращения дроби).
+            var newDenominator = denominator / greatestCommonDivisor;
+
+            // Выводим новый числитель и знаменятель на экран.
+            Console.WriteLine(newNumerator + " " + newDenominator);
+        }
+    }
+}
